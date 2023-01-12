359f6fc92c65ceab193e4dad2ff10b7e4ad3f8d4
ednlpz <e.lopez103199@gmail.com>
2023-01-12 12:13:56 +0800

    Added support for infinite querying
    - Added infinite querying function on scroll down.
    - Added mock data for tests.
    - Updated react query to version 4.
    - Reworked test for pokemon list component.
    - Exported useInfiniteQuery to its own custom hook (useInfinitePokemonQuery).

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -2,12 +2,18 @@ import { API_BASE_URL } from "common/environment";
 
 //opted to not use axios to lessen app size
 
-export const getAllPokemon: (limit?: number) => Promise<{
+export const getAllPokemon: (
+  page?: string,
+  limit?: number
+) => Promise<{
   result: any;
   nextPage: string | null;
   prevPage: string | null;
-}> = async (limit = 8) => {
-  const response = await fetch(`${API_BASE_URL}/pokemon?limit=${limit}`);
+}> = async (page, limit = 20) => {
+  // page is returned by the API under data.results, it is for loading the nextPage or the previousPage
+  const response = await fetch(
+    page ? page : `${API_BASE_URL}/pokemon?limit=${limit}`
+  );
 
   if (!response.ok) {
     console.error(`HTTP error! status: ${response.status}`);
--- src/features/pokemon-list/index.tsx
+++ src/features/pokemon-list/index.tsx
@@ -1,46 +1,82 @@
-import { getAllPokemon } from "api/pokemon";
-import { useQuery } from "react-query";
-
 import CardList from "components/card-list";
 import Card from "components/card";
 import Loading from "components/loading";
+import { Fragment, useEffect } from "react";
+import useInfinitePokemonQuery from "./hooks/useInfinitePokemonQuery";
 const PokemonList = () => {
-  const getPokemonQuery = useQuery("todos", () => getAllPokemon());
+  const { data, hasNextPage, isLoading, fetchNextPage, isFetchingNextPage } =
+    useInfinitePokemonQuery();
 
-  if (getPokemonQuery.isLoading) {
-    return (
-      <div className='grid place-content-center w-full'>
-        <Loading className='w-6 h-6' />
-      </div>
-    );
+  useEffect(() => {
+    let fetchingSignal = false;
+
+    const onScroll = async (
+      event: any
+      /* This throws a warning on line 46 & 50
+          React.UIEvent<Document> & {
+            target: {
+              scrollingElement: {
+                scrollHeight: number;
+                scrollTop: number;
+                clientHeight: number;
+              };
+            };
+          }
+     */
+    ) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        event.target.scrollingElement;
+
+      if (!fetchingSignal && scrollTop + clientHeight * 1.25 >= scrollHeight) {
+        fetchingSignal = true;
+        if (hasNextPage) await fetchNextPage();
+        fetchingSignal = false;
+      }
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, [hasNextPage]);
+
+  if (isLoading) {
+    return <LoaderWrapper />;
   }
+
   return (
-    <CardList className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full'>
-      {/* 
-      // Pokemon is of type any
-      // are there ways to get the type schema of the data from the API
-      // or we have to manually declare the schema?
-      */}
-      {getPokemonQuery.data?.result.map((pokemon: any, index: number) => {
+    <CardList className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full relative'>
+      {data?.pages.map((pageGroup: any, groupIndex: number) => {
         return (
-          <Card
-            className='justify-self-center'
-            key={`${pokemon.name}-${index}`}
-          >
-            <Card.Image
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                index + 1
-              }.png`}
-              alt={pokemon.name}
-            />
-            <Card.Name className='p-2 first-letter:uppercase text-center'>
-              {pokemon.name}
-            </Card.Name>
-          </Card>
+          <Fragment key={`${groupIndex}`}>
+            {pageGroup.result.map((pokemon: any, pokemonIndex: number) => {
+              return (
+                <Card className='justify-self-center' key={`${pokemon.name}`}>
+                  <Card.Image
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                      groupIndex * 20 + pokemonIndex + 1
+                    }.png`}
+                    alt={pokemon.name}
+                  />
+                  <Card.Name className='p-2 first-letter:uppercase text-center'>
+                    {pokemon.name}
+                  </Card.Name>
+                </Card>
+              );
+            })}
+          </Fragment>
         );
       })}
+      {isFetchingNextPage && <LoaderWrapper />}
     </CardList>
   );
 };
+
+const LoaderWrapper = () => (
+  <div className='grid place-content-center col-span-full w-full'>
+    <Loading className='w-6 h-6' />
+  </div>
+);
 
 export default PokemonList;
